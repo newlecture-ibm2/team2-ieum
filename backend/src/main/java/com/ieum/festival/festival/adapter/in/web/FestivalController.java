a778e9f45778c559a459e9c5ff63805ded31720a
채나은 <coskdms95@gmail.com>
2026-03-29 17:18:51 +0900

    Revert "refactor: festival → event 도메인명 변경 (프로젝트 패키지명 중복 해소)"
    This reverts commit 91f34331f63ddbd7de6791ce5737fe86730864e2.

--- backend/src/main/java/com/ieum/festival/festival/adapter/in/web/FestivalController.java
+++ backend/src/main/java/com/ieum/festival/festival/adapter/in/web/FestivalController.java
@@ -1,4 +1,4 @@
-package com.ieum.festival.event.adapter.in.web;
+package com.ieum.festival.festival.adapter.in.web;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
