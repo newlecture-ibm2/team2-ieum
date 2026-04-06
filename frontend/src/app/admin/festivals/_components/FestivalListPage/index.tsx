d36fdae556c020aeb0c32f5943631954bc88f983
skpark0205 <skpark0205@gmail.com>
2026-04-06 17:17:06 +0900

    style: admin festival modal refactor, header text align, common modal padding

--- frontend/src/app/admin/festivals/_components/FestivalListPage/index.tsx
+++ frontend/src/app/admin/festivals/_components/FestivalListPage/index.tsx
@@ -250,9 +250,9 @@ export default function FestivalListPage() {
               <th className={`${c.tableHeaderCell} ${c.textLeft}`}>축제명</th>
               <th className={`${c.tableHeaderCell} ${c.textLeft}`}>카테고리</th>
               <th className={`${c.tableHeaderCell} ${c.textLeft}`}>지역</th>
-              <th className={`${c.tableHeaderCell} ${c.textLeft}`}>날짜</th>
-              <th className={`${c.tableHeaderCell} ${c.textLeft}`}>상태</th>
-              <th className={`${c.tableHeaderCell} ${c.textRight}`}>관리 (노출/숨김)</th>
+              <th className={`${c.tableHeaderCell} ${c.textCenter}`}>날짜</th>
+              <th className={`${c.tableHeaderCell} ${c.textCenter}`}>상태</th>
+              <th className={`${c.tableHeaderCell} ${c.textCenter}`}>관리 (노출/숨김)</th>
             </tr>
           </thead>
           <tbody>
